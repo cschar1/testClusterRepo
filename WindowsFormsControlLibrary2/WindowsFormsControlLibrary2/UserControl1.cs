d368994ec1101aeebb52ce04ab789a53227b4397
unknown <cody.scharfe@ntc.norbord.com>
2010-06-07 10:25:43 -0400

    Initial Commit , Added both main application and its windowsFormControlLibrary

---
+++ WindowsFormsControlLibrary2/WindowsFormsControlLibrary2/UserControl1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary2
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            timer1.Interval = 150;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void UserControl1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawEllipse(Pens.Black, new Rectangle(20, 20, 90, 30));
+
+        }
+    }
+}
